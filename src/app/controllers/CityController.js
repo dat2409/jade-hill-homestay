bddf73ec6e6cdfc7e6a57ced3c30e40932f1b8bb
huongctt <thanhhuong12333@gmail.com>
2021-11-25 09:07:39 +0700

    CRUD city and edit homestay 2

---
+++ src/app/controllers/CityController.js
@@ -0,0 +1,65 @@
+const City = require('../models/city');
+const mongoose = require('../../util/mongoose');
+const Homestay = require('../models/homestay');
+
+class CityController {
+  //GET /cities/new
+  create(req, res, next) {
+    res.send('create city');
+  }
+
+  //POST /cities/create
+  async postCreate(req, res, next) {
+    const city = new City({
+        ...req.body
+    })
+    city.save()
+    res.send(city)
+  }
+
+  async delete(req, res, next) {
+    const city = await City.findByIdAndDelete({_id: req.params.id})
+    const homestays = await Homestay.find({city: req.params.id})
+    for (var i = 0; i < homestays.length; i++){
+      await Homestay.findByIdAndDelete({_id: homestays[i]._id})
+    }
+    console.log(homestays)
+    res.send("delete successfully")
+  }
+
+  async getCity(req, res, next) {
+    const city = await City.findById(req.params.id)
+    await city.populate({
+      path:'homestays'
+    }).execPopulate()
+    res.send(city)
+  }
+
+  async getUpdate(req, res, next) {
+    const city = await City.findById(req.params.id)
+    res.send(city)
+  }
+
+  async postUpdate(req, res, next) {
+    const updates = Object.keys(req.body)
+    const city = await City.findById(req.params.id)
+    try {
+        updates.forEach((update) => city[update] = req.body[update])
+        await city.save()
+        res.send(city)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+  }
+
+  async all(req, res, next) {
+    City.find()
+        .then(cities => {
+            console.log(cities);
+            res.json(cities)
+        })
+        .catch(err => console.log(err));
+  }
+}
+
+module.exports = new CityController();
--- src/app/models/city.js
+++ src/app/models/city.js
@@ -6,6 +6,21 @@ const Schema = new mongoose.Schema({
     }
 },{timestamps: true});
 
+Schema.virtual('homestays', {
+    ref: 'Homestay',
+    localField: '_id',
+    foreignField: 'city'
+})
+
+Schema.methods.toJSON = function() {
+    const city = this
+    const cityObject = city.toObject()
+
+    delete cityObject.createdAt
+    delete cityObject.updatedAt
+    return cityObject
+}
+
 const  City = mongoose.model('City', Schema);
 
 module.exports = City;
