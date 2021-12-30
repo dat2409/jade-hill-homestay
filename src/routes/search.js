293e31a6268389fb05a0e1542281ad5593390c28
huongctt <thanhhuong12333@gmail.com>
2021-12-30 21:53:42 +0700

    search homestay by name and get homestay by id - for visitor

--- src/routes/search.js
+++ src/routes/search.js
@@ -8,5 +8,7 @@ const router = express.Router();
 
 router.get('/cities', searchController.getAllCities);
 router.get('/', searchController.search);
+//get homestay info and service
+router.get('/homestays/:id', searchController.getOneHomestay);
 
 module.exports = router;
